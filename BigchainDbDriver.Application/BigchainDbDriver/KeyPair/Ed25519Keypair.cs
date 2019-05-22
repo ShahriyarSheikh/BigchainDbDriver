e0a7f245f6e07b5fd3025c9b750e9999b2a0093c
Shahriyar Sheikh <m.shahriyarsheikh@gmail.com>
2019-05-22 11:34:31 +0500

    --Commit: added interface in ed25519 keypair

--- BigchainDbDriver.Application/BigchainDbDriver/KeyPair/Ed25519Keypair.cs
+++ BigchainDbDriver.Application/BigchainDbDriver/KeyPair/Ed25519Keypair.cs
@@ -6,14 +6,15 @@ using BigchainDbDriver.Assets.Models;
 
 namespace BigchainDbDriver.KeyPair
 {
-    public class Ed25519Keypair
+    public class Ed25519Keypair : IEd25519Keypair
     {
         private readonly int bytesSupportedbyEd25519 = 32;
         public Ed25519Keypair()
         {
 
         }
-        public GeneratedKeyPair GenerateKeyPair(byte[] seed = null) {
+        public GeneratedKeyPair GenerateKeyPair(byte[] seed = null)
+        {
             DataEncoder encoder = Encoders.Base58;
 
             var byt = seed == null ? RandomUtils.GetBytes(bytesSupportedbyEd25519) : seed;
@@ -21,12 +22,13 @@ namespace BigchainDbDriver.KeyPair
             var pk = Ed25519.PublicKeyFromSeed(byt);
 
             var sk = Ed25519.ExpandedPrivateKeyFromSeed(byt);
-            
+
             // tweetnacl's generated secret key is the secret key + public key (resulting in a 64-byte buffer)
             // therefore require slicing
             var _sk = Slice(sk, bytesSupportedbyEd25519);
 
-            return new GeneratedKeyPair {
+            return new GeneratedKeyPair
+            {
                 PrivateKey = encoder.EncodeData(_sk),
                 PublicKey = encoder.EncodeData(pk),
                 ExpandedPrivateKey = encoder.EncodeData(sk)
---
+++ BigchainDbDriver.Application/BigchainDbDriver/KeyPair/IEd25519Keypair.cs
@@ -0,0 +1,9 @@
+﻿using BigchainDbDriver.Assets.Models;
+
+namespace BigchainDbDriver.KeyPair
+{
+    public interface IEd25519Keypair
+    {
+        GeneratedKeyPair GenerateKeyPair(byte[] seed = null);
+    }
+}
